b6d149c946994069a7f846cd49384704c51171ff
bulletcross <bulletcross@gmail.com>
2018-07-24 22:34:10 +0530

    training in thread

--- trainer.py
+++ trainer.py
@@ -24,7 +24,10 @@ This module is GPLv3 licensed.
 """
 
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 import os
+import threading
+import time
 
 """
 A model trainer encapsulate data, model and
@@ -59,9 +62,25 @@ class model_trainer:
     def train(self, collect_stats = True):
         if self.is_training == True:
             return
+
+        sess = tf.Session()
+        y = self.model['output_tensor']
+        y_ = tf.placeholder(tf.float32, shape=?)
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+        train_op = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+        # For stats
+        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # Train script
         # Train model on data based on params, in one thread
+        def training_func():
+            while True:
+                batch = mnist.train.next_batch(100)
+                global_step_val,_ = sess.run([global_step, train_step], feed_dict={x: batch[0], y_: batch[1]})
+                print("global step: %d" % global_step_val)
+        threading.Thread(target=training_func).start()
         self.is_training = True
-        # Format and save train stats
+        # Format and save train stats based on accuracy data
         self.train_stats = None
         self.is_training = False
 
